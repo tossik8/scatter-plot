808f33f9a79854e889b7c34e6e4628f903b891ff
user <toropov.nikita2004@gmail.com>
2023-02-01 20:15:05 +0100

    Create axes

--- scatter-plot.js
+++ scatter-plot.js
@@ -7,9 +7,10 @@ fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
 
 function createPlot(data){
 
-    console.log(data[0])
+    console.log(data)
     const width = 920;
     const height = 630;
+    const margins = {top: 20, right: 20, bottom: 20, left: 40};
 
     const svg = d3.select(".panel")
       .append("svg")
@@ -18,21 +19,22 @@ function createPlot(data){
 
 
     const xScale = d3.scaleLinear()
-                     .domain(d3.extent(data, d => d["Year"]))
-                     .range([20, width]);
+                     .domain([d3.min(data, d => d["Year"]) - 1, d3.max(data, d => d["Year"]) + 1])
+                     .range([margins.left, width - margins.right]);
 
     const yScale = d3.scaleLinear()
-                     .domain([d3.min(data, d => d["Seconds"]), d3.max(data, d => d["Seconds"])])
-                     .range([0, 600]);
+                     .domain(d3.extent(data, d => d["Seconds"]))
+                     .range([0, height - margins.bottom]);
 
     const xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d"));
     const yAxis = d3.axisLeft(yScale).tickFormat(formatTick);
 
     svg.append("g")
+       .attr("transform", `translate(0, ${height - margins.bottom})`)
        .call(xAxis);
 
     svg.append("g")
-       .attr("transform", "translate(40, 20)")
+       .attr("transform", `translate(${margins.left}, 0)`)
        .call(yAxis);
 }
 function formatTick(seconds){
